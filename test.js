2173e09b6bcdbc2f624b14ae11174cf973091079
Kevin Swiber <kswiber@gmail.com>
2013-05-21 13:02:16 -0400

    Cleaning up.

--- test.js
+++ test.js
@@ -1,18 +1,16 @@
-var Parser = require('./surface').Parser;
+var query = require('./query');
 var Ast = require('./ast');
 
-var parser = new Parser();
-
-parser.yy = Ast;
+query.parser.yy = Ast;
 
 var parsed;
 
-parsed = parser.parse('SELECT * WHERE id=2');
+/*parsed = query.parse('SELECT * WHERE id=2');
 console.log(parsed);
 
-parsed = parser.parse('SELECT * WHERE name eq \'Kevin\' AND facebook.title=\'The Dude\' AND id=2');
-console.log(parsed);
+parsed = query.parse('SELECT * WHERE name eq \'Kevin\' AND facebook.title=\'The Dude\' AND id=2');
+console.log(parsed);*/
 
 
-parsed = parser.parse('SELECT first_name, last_name, favorite_food, of WHERE name eq \'Kevin\' AND NOT location.coordinates within .5 of 40.042016, -86.900749 AND NOT id=2 && NOT name contains \'foo%\' && of=\'hi\' OR a=1');
+parsed = query.parse('SELECT first_name, contains,last_name, favorite_food, of WHERE name eq \'Kevin\' AND NOT location.coordinates within .5 of 40.042016, -86.900749 AND NOT id=2 && NOT name contains \'foo%\' && of=\'hi\' OR a=1');
 console.log(parsed);
